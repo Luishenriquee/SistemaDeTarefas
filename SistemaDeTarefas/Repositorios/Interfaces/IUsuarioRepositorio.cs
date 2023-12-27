5c7bc5c4a0c8895b7172a7b75227a844233c8685
Luis Henrique <Luis Henrique@DESKTOP-BG0MEHK>
2023-12-26 22:42:56 -0300

    #Instalação dos pacotes e criação da estrutura do repositorio

---
+++ SistemaDeTarefas/Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repositorios.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        Task<List<UsuarioModel>> BuscarTodosUsuarios();
+
+        Task<UsuarioModel> BuscarPorId(int id);
+
+        Task<UsuarioModel> AdicionarUsuario(UsuarioModel usuario);
+
+        Task<UsuarioModel> AtualizarUsuario(UsuarioModel usuario, int id);
+
+        Task<bool> RemoverUsuario(int id);
+    }
+}
