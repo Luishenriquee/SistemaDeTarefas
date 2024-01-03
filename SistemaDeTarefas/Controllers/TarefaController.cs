2813b5d15bcd1b6ca9b4eb6b079936874ea476ea
Luishenriquee <iluishenriquebr@gmail.com>
2024-01-02 23:20:20 -0300

    Adição do método BuscarTarefas

--- SistemaDeTarefas/Controllers/TarefaController.cs
+++ SistemaDeTarefas/Controllers/TarefaController.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaDeTarefas.Models;
 
 namespace SistemaDeTarefas.Controllers
 {
     public class TarefaController : ControllerBase
     {
+        [HttpGet]
+        public async Task<ActionResult<List<TarefaModel>>> BuscarTarefas()
+        {
+            return Ok();
+        }
     }
 }
