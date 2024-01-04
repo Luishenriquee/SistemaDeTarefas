7e39088f402047f672f80271c28e056ed17c8c09
Luishenriquee <iluishenriquebr@gmail.com>
2024-01-03 22:27:12 -0300

    Criação do CRUD de Tarefas e vinculo com usuário

---
+++ SistemaDeTarefas/Repositorios/Interfaces/ITarefaRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repositorios.Interfaces
+{
+    public interface ITarefaRepositorio
+    {
+        Task<List<TarefaModel>> BuscarTodosTarefa();
+
+        Task<TarefaModel> BuscarPorId(int id);
+
+        Task<TarefaModel> AdicionarTarefa(TarefaModel tarefa);
+
+        Task<TarefaModel> AtualizarTarefa(TarefaModel tarefa, int id);
+
+        Task<bool> RemoverTarefa(int id);
+    }
+}
---
+++ SistemaDeTarefas/Repositorios/TarefaRepositorio.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositorios.Interfaces;
+
+namespace SistemaDeTarefas.Repositorios
+{
+    public class TarefaRepositorio : ITarefaRepositorio
+    {
+        private readonly SistemaTarefasDBContext _dbContext;
+
+        public TarefaRepositorio(SistemaTarefasDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+
+        public async Task<TarefaModel> AdicionarTarefa(TarefaModel tarefa)
+        {
+            await _dbContext.Tarefas.AddAsync(tarefa);
+            await _dbContext.SaveChangesAsync();
+
+            return tarefa;
+        }
+
+        public async Task<TarefaModel> AtualizarTarefa(TarefaModel tarefa, int id)
+        {
+            TarefaModel tarefaId = await BuscarPorId(id);
+            if (tarefaId is null)
+                throw new Exception($"Tarefa com identificador: {id}, não foi encontrado!");
+
+            tarefaId.Name = tarefa.Name;
+            tarefaId.Status = tarefa.Status;
+            tarefaId.Descricao = tarefa.Descricao;
+            tarefaId.Status = tarefa.Status;
+            tarefaId.UsuarioId = tarefa.UsuarioId;
+
+            _dbContext.Tarefas.Update(tarefaId);
+            await _dbContext.SaveChangesAsync();
+
+            return tarefaId;
+        }
+
+        public async Task<TarefaModel> BuscarPorId(int id)
+        {
+            return await _dbContext.Tarefas
+                .Include(x => x.Usuario)
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<TarefaModel>> BuscarTodosTarefa()
+        {
+            return await _dbContext.Tarefas
+                .Include(x => x.Usuario)
+                .ToListAsync();
+        }
+
+        public async Task<bool> RemoverTarefa(int id)
+        {
+            TarefaModel tarefaId = await BuscarPorId(id);
+            if (tarefaId is null)
+                throw new Exception($"Tarefa com identificador: {id}, não foi encontrado!");
+
+            _dbContext.Tarefas.Remove(tarefaId);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+    }
+}
